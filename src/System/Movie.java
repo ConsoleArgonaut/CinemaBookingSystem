d991b69630b12f29ef601c4e44fb329c77204f6e
ConsoleArgonaut <vanderheide.michael@gmail.com>
2017-06-23 13:56:36 +0200

    Added properties to BookingSystem, Client, Movie and Seat

--- src/System/Movie.java
+++ src/System/Movie.java
@@ -4,4 +4,11 @@ package System;
  * Created by Michael on 23.06.2017.
  */
 public class Movie {
+    public String title;
+
+    public String description;
+
+    public String trailer;
+
+    public int lenght;
 }
--- src/System/Seat.java
+++ src/System/Seat.java
@@ -4,4 +4,33 @@ package System;
  * Created by Michael on 23.06.2017.
  */
 public class Seat {
+    private int seatnumber;
+
+    private int rownumber;
+
+    private Client client;
+
+    public int getSeatnumber() {
+        return seatnumber;
+    }
+
+    public void setSeatnumber(int seatnumber) {
+        this.seatnumber = seatnumber;
+    }
+
+    public int getRownumber() {
+        return rownumber;
+    }
+
+    public void setRownumber(int rownumber) {
+        this.rownumber = rownumber;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
 }
