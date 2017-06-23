453052f7309b0718b6f89b4a1c3e81bd32789861
DESKTOP-I2OVG7C\Drake <kaiden3991@gmail.com>
2017-06-23 13:57:01 +0200

    Added basic variables and methodes without functionality

--- src/System/Show.java
+++ src/System/Show.java
@@ -1,7 +1,12 @@
 package System;
 
+import java.util.Date;
+
 /**
  * Created by Michael on 23.06.2017.
  */
 public class Show {
+    private Movie movie;
+    private Date startTime;
+    private Theatre theatre;
 }
--- src/System/Theatre.java
+++ src/System/Theatre.java
@@ -4,4 +4,9 @@ package System;
  * Created by Michael on 23.06.2017.
  */
 public class Theatre {
+    private String theatrenumber;
+    private Seat[] seats;
+
+    public Theatre copy(){ return null;}
 }
+
