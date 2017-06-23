33a25ef3a66e917407e5bcef7c4b07b3bbf8a5d7
ConsoleArgonaut <vanderheide.michael@gmail.com>
2017-06-23 14:31:42 +0200

    Added functionality to BookingSystem

--- src/System/BookingSystem.java
+++ src/System/BookingSystem.java
@@ -10,19 +10,60 @@ public class BookingSystem {
     private ArrayList<Theatre> theatres;
 
     public Movie[] getAllCurrentMovies() {
-        return new Movie[0];
+        ArrayList<Movie> movies = new ArrayList<Movie>();
+        for (Show s:shows) {
+            if(movies.size() == 0)
+                movies.add(s.getMovie());
+            else if (!movies.contains(s.getMovie()))
+                movies.add(s.getMovie());
+        }
+
+        return (Movie[]) movies.toArray();
     }
 
-    public void bookSeats(Client client, Show show, int rownumber, int searnumberFrom, int seatnumberTo){
-
+    public void bookSeats(Client client, Show show, int rownumber, int seatnumberFrom, int seatnumberTo){
+        if(!shows.contains(show))
+            throw new IllegalArgumentException("Show does not exists");
+        else{
+            ArrayList<Seat> usedSeats = new ArrayList<Seat>();
+            for (Seat s:show.getTheatre().getSeats()) {
+                if(s.getRownumber() == rownumber && seatnumberFrom <= s.getSeatnumber() && seatnumberTo >= s.getSeatnumber()){
+                    if(s.getClient() != null)
+                        throw new IllegalArgumentException("Some seats already taken");
+                    else
+                        usedSeats.add(s);
+                }
+            }
+            for (Seat s:usedSeats) {
+                s.setClient(client);
+            }
+        }
     }
 
     public void cancelReservation(String phonenumber, Show show){
-
+        for (Seat s:show.getTheatre().getSeats()) {
+            if(s.getClient().getPhonenumber() == phonenumber)
+                s.setClient(null);
+        }
     }
 
     public void addShow(Show show){
-
+        shows.add(show);
     }
 
+    public void setShows(ArrayList<Show> shows) {
+        this.shows = shows;
+    }
+
+    public ArrayList<Show> getShows(){
+        return shows;
+    }
+
+    public ArrayList<Theatre> getTheatres() {
+        return theatres;
+    }
+
+    public void setTheatres(ArrayList<Theatre> theatres) {
+        this.theatres = theatres;
+    }
 }
