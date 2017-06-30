5cf9b7f31a863ab12eec3b22e33bffef39cca47d
ConsoleArgonaut <vanderheide.michael@gmail.com>
2017-06-30 14:26:10 +0200

    Added some comfort functionality

--- src/System/BookingSystem.java
+++ src/System/BookingSystem.java
@@ -11,8 +11,8 @@ public class BookingSystem {
 
     public Movie[] getAllCurrentMovies() {
         ArrayList<Movie> movies = new ArrayList<Movie>();
-        for (Show s:shows) {
-            if(movies.size() == 0)
+        for (Show s : shows) {
+            if (movies.size() == 0)
                 movies.add(s.getMovie());
             else if (!movies.contains(s.getMovie()))
                 movies.add(s.getMovie());
@@ -21,33 +21,33 @@ public class BookingSystem {
         return (Movie[]) movies.toArray();
     }
 
-    public void bookSeats(Client client, Show show, int rownumber, int seatnumberFrom, int seatnumberTo){
-        if(!shows.contains(show))
+    public void bookSeats(Client client, Show show, int rownumber, int seatnumberFrom, int seatnumberTo) {
+        if (!shows.contains(show))
             throw new IllegalArgumentException("Show does not exists");
-        else{
+        else {
             ArrayList<Seat> usedSeats = new ArrayList<Seat>();
-            for (Seat s:show.getTheatre().getSeats()) {
-                if(s.getRownumber() == rownumber && seatnumberFrom <= s.getSeatnumber() && seatnumberTo >= s.getSeatnumber()){
-                    if(s.getClient() != null)
+            for (Seat s : show.getTheatre().getSeats()) {
+                if (s.getRownumber() == rownumber && seatnumberFrom <= s.getSeatnumber() && seatnumberTo >= s.getSeatnumber()) {
+                    if (s.getClient() != null)
                         throw new IllegalArgumentException("Some seats already taken");
                     else
                         usedSeats.add(s);
                 }
             }
-            for (Seat s:usedSeats) {
+            for (Seat s : usedSeats) {
                 s.setClient(client);
             }
         }
     }
 
-    public void cancelReservation(String phonenumber, Show show){
-        for (Seat s:show.getTheatre().getSeats()) {
-            if(s.getClient().getPhonenumber() == phonenumber)
+    public void cancelReservation(String phonenumber, Show show) {
+        for (Seat s : show.getTheatre().getSeats()) {
+            if (s.getClient().getPhonenumber() == phonenumber)
                 s.setClient(null);
         }
     }
 
-    public void addShow(Show show){
+    public void addShow(Show show) {
         shows.add(show);
     }
 
@@ -55,7 +55,7 @@ public class BookingSystem {
         this.shows = shows;
     }
 
-    public ArrayList<Show> getShows(){
+    public ArrayList<Show> getShows() {
         return shows;
     }
 
@@ -65,5 +65,31 @@ public class BookingSystem {
 
     public void setTheatres(ArrayList<Theatre> theatres) {
         this.theatres = theatres;
+    }
+
+    public ArrayList<Show> getReservations(String phonenumber) {
+        ArrayList<Show> showsToReturn = new ArrayList<Show>();
+        for (Show show : shows) {
+            for (Seat seat : show.getTheatre().getSeats()) {
+                if(showsToReturn.contains(show) && seat.getClient().getPhonenumber() == phonenumber)
+                    showsToReturn.add(show);
+            }
+        }
+        return showsToReturn;
+    }
+
+    public ArrayList<Show> getReservations(Client client) {
+        return getReservations(client.getPhonenumber());
+    }
+
+    public Client getClient(String firstName, String lastName) {
+        Client client = null;
+        for (Show show : shows) {
+            for (Seat seat : show.getTheatre().getSeats()) {
+                if(client == null && seat.getClient().getFirstname() == firstName && seat.getClient().getLastname() == lastName)
+                    client = seat.getClient();
+            }
+        }
+        return client;
     }
 }
