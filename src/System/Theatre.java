b02c8026fb1f11f1c4100eb08564713cb94290a7
ConsoleArgonaut <vanderheide.michael@gmail.com>
2017-07-04 13:50:33 +0200

    Added getAllOpenSeats function to theater

--- src/System/Theatre.java
+++ src/System/Theatre.java
@@ -46,5 +46,30 @@ public class Theatre implements Serializable {
         theatrenumber = theatrenumberToSet;
         seats = seatsToSet;
     }
+
+    public String getAllOpenSeats(){
+        String openSeats = "";
+
+        boolean rowPrinted = false;
+
+        for (int rowNumber = 0; rowNumber < 100; rowNumber++) {
+            for (int seatNumber = 0; seatNumber < 100; seatNumber++) {
+                for (Seat seat:seats) {
+                    if(seat.getRownumber() == rowNumber && seat.getSeatnumber() == seatNumber && seat.getClient() == null){
+                        if(rowPrinted)
+                            openSeats = openSeats + ", " + String.valueOf(seatNumber);
+                        else
+                            openSeats = openSeats + "Row " + String.valueOf(rowNumber) + ": " + String.valueOf(seatNumber);
+                        rowPrinted = true;
+                    }
+                }
+            }
+            if(rowPrinted)
+                openSeats = openSeats + "\n";
+            rowPrinted = false;
+        }
+
+        return openSeats;
+    }
 }
 
