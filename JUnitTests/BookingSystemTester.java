1be7287d51c96a76ed810987385a846c7c6fcdcc
ConsoleArgonaut <vanderheide.michael@gmail.com>
2017-07-04 08:56:35 +0200

    Added createDefaultCinema to Tests and Edited save and Load test to use this feature

--- JUnitTests/BookingSystemTester.java
+++ JUnitTests/BookingSystemTester.java
@@ -45,34 +45,8 @@ public class BookingSystemTester {
 
     @Test
     public void saveAndLoad() {
-        BookingSystem system = new BookingSystem();
-        //Creates a theatre with 2 seats and adds it to the system
-        Theatre theatre = new Theatre();
-        theatre.setTheatrenumber("123");
-        Seat[] seats = new Seat[2];
-        seats[0] = new Seat();
-        seats[0].setSeatnumber(1);
-        seats[0].setRownumber(1);
-        seats[1] = new Seat();
-        seats[1].setSeatnumber(2);
-        seats[1].setRownumber(1);
-        theatre.setSeats(seats);
-        system.getTheatres().add(theatre);
-        //Creates 2 test shows
-        Show show1 = new Show();
-        Movie movie = new Movie();
-        movie.description = "test";
-        movie.length = 20;
-        movie.title = "Testikus";
-        show1.setMovie(movie);
-        show1.setStartTime(Date.valueOf("2007-12-25"));
-        show1.setTheatre(theatre.copy());
-        Show show2 = new Show();
-        show2.setMovie(movie);
-        show2.setStartTime(Date.valueOf("2007-12-26"));
-        show2.setTheatre(theatre.copy());
-        system.getShows().add(show1);
-        system.getShows().add(show2);
+        //Gets Default System
+        BookingSystem system = createDefaultCinema();
 
         //Saves
         system.save();
@@ -80,9 +54,21 @@ public class BookingSystemTester {
         //Loads
         BookingSystem systemLoaded = new BookingSystem();
         systemLoaded.load();
+
         //Checks some values to see if saving was successful
-        Assert.assertEquals(systemLoaded.getShows().get(0).getMovie().title, "Testikus");
-        Assert.assertEquals(systemLoaded.getShows().get(1).getStartTime(), Date.valueOf("2007-12-26"));
-        Assert.assertEquals(systemLoaded.getTheatres().get(0).getSeats().length, 2);
+        Assert.assertEquals(systemLoaded.getShows().get(0).getMovie().title, system.getShows().get(0).getMovie().title);
+        Assert.assertEquals(systemLoaded.getShows().get(1).getStartTime(), system.getShows().get(1).getStartTime());
+        Assert.assertEquals(systemLoaded.getTheatres().get(0).getSeats().length, system.getTheatres().get(0).getSeats().length);
+    }
+
+    private BookingSystem createDefaultCinema(){
+        BookingSystem cinema = new BookingSystem();
+        cinema.getTheatres().add(new Theatre("Theatre 1", new Seat[] {new Seat(1, 1), new Seat(1, 2), new Seat(1, 3), new Seat(2, 1), new Seat(2, 2), new Seat(2, 3)}));
+        cinema.getTheatres().add(new Theatre("Theatre 2", new Seat[] {new Seat(1, 1), new Seat(1, 2), new Seat(1, 3), new Seat(2, 1), new Seat(2, 2), new Seat(2, 3)}));
+        cinema.getTheatres().add(new Theatre("Theatre 3 - Kids", new Seat[] {new Seat(1, 1), new Seat(1, 2), new Seat(1, 3)}));
+        cinema.getShows().add(new Show(new Movie("RoboCop", "Der Polizist Alex Murphy wird bei einem Einsatz brutal ermordet und erwacht im Körper eines Roboters zu neuem Leben.", 103, "https://www.youtube.com/watch?v=zbCbwP6ibR4"), Date.valueOf("2018-08-25"), cinema.getTheatres().get(0).copy()));
+        cinema.getShows().add(new Show(new Movie("Die Hochzeits-Crasher", "Die Hochzeits-Crasher ist eine Filmkomödie des Regisseurs David Dobkin aus dem Jahre 2005 mit Owen Wilson und Vince Vaughn.", 123, "https://www.youtube.com/watch?v=pMmz4IZTjq8"), Date.valueOf("2018-12-07"), cinema.getTheatres().get(1).copy()));
+        cinema.getShows().add(new Show(new Movie("High School Musical", "High School Musical ist ein US-amerikanischer Musical-Fernsehfilm der Walt Disney Company aus dem Jahr 2006.", 98, "https://www.youtube.com/watch?v=ukDLkkvZYFk"), Date.valueOf("2018-02-13"), cinema.getTheatres().get(2).copy()));
+        return cinema;
     }
 }
