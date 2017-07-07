0997e611bb587e8b02caf05403294b3c89b073db
ConsoleArgonaut <vanderheide.michael@gmail.com>
2017-07-07 13:11:48 +0200

    Added goto functions to main

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,8 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,14 +12,62 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("starter.fxml"));
+        primaryStage.setTitle("Booking System");
+        primaryStage.setScene(new Scene(root, 900, 600));
+        primaryStage.setResizable(false);
         primaryStage.show();
+        PrimaryStage = primaryStage;
     }
 
+    /*Properties*/
+    private static Stage PrimaryStage;
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @FXML
+    /*This sets starter as current scene*/
+    public void goToStarter(Event arg0) {
+        try{
+            PrimaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("race_selection.fxml")), 900, 600));
+        }
+        catch (Exception ex){
+
+        }
+    }
+
+    @FXML
+    /*This sets bookseats as current scene*/
+    public void goToBookSeats(Event arg0) {
+        try{
+            PrimaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("bookseats.fxml")), 900, 600));
+        }
+        catch (Exception ex){
+
+        }
+    }
+
+    @FXML
+    /*This sets cancelreservation as current scene*/
+    public void goToCancelReservation(Event arg0) {
+        try{
+            PrimaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("cancelreservation.fxml")), 900, 600));
+        }
+        catch (Exception ex){
+
+        }
+    }
+
+    @FXML
+    /*This sets addshow as current scene*/
+    public void goToAddShow(Event arg0) {
+        try{
+            PrimaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("addshow.fxml")), 900, 600));
+        }
+        catch (Exception ex){
+
+        }
     }
 }
