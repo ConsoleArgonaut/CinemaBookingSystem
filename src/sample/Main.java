cb8d5051bcb9cbad562b05017b1f0140e6db2959
ConsoleArgonaut <vanderheide.michael@gmail.com>
2017-07-10 14:07:06 +0200

    Book seats has list of shows

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,8 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,6 +14,8 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import System.*;
 
+import java.sql.Date;
+
 public class Main extends Application {
 
     @Override
@@ -22,10 +26,21 @@ public class Main extends Application {
         primaryStage.setResizable(false);
         primaryStage.show();
         PrimaryStage = primaryStage;
+        system = new BookingSystem();
+
+        system.getTheatres().add(new Theatre("Theatre 1", new Seat[] {new Seat(1, 1), new Seat(1, 2), new Seat(1, 3), new Seat(2, 1), new Seat(2, 2), new Seat(2, 3)}));
+        system.getTheatres().add(new Theatre("Theatre 2", new Seat[] {new Seat(1, 1), new Seat(1, 2), new Seat(1, 3), new Seat(2, 1), new Seat(2, 2), new Seat(2, 3)}));
+        system.getTheatres().add(new Theatre("Theatre 3 - Kids", new Seat[] {new Seat(1, 1), new Seat(1, 2), new Seat(1, 3)}));
+        system.getShows().add(new Show(new Movie("RoboCop", "Der Polizist Alex Murphy wird bei einem Einsatz brutal ermordet und erwacht im Körper eines Roboters zu neuem Leben.", 103, "https://www.youtube.com/watch?v=zbCbwP6ibR4"), Date.valueOf("2018-08-25"), system.getTheatres().get(0).copy()));
+        system.getShows().add(new Show(new Movie("Die Hochzeits-Crasher", "Die Hochzeits-Crasher ist eine Filmkomödie des Regisseurs David Dobkin aus dem Jahre 2005 mit Owen Wilson und Vince Vaughn.", 123, "https://www.youtube.com/watch?v=pMmz4IZTjq8"), Date.valueOf("2018-12-07"), system.getTheatres().get(1).copy()));
+        system.getShows().add(new Show(new Movie("High School Musical", "High School Musical ist ein US-amerikanischer Musical-Fernsehfilm der Walt Disney Company aus dem Jahr 2006.", 98, "https://www.youtube.com/watch?v=ukDLkkvZYFk"), Date.valueOf("2018-02-13"), system.getTheatres().get(2).copy()));
+
     }
 
     /*Properties*/
     private static BookingSystem system;
+    private static boolean isFirstInitialize = true;
+
 
     private static Stage PrimaryStage;
 
@@ -115,7 +130,15 @@ public class Main extends Application {
 
     @FXML
     private void initialize(){
-        if(bookseats_openRows != null)
-            bookseats_openRows.setText(String.valueOf("Row 1: x"));
+        if(isFirstInitialize)
+            isFirstInitialize = false;
+        else{
+            if(bookseats_openRows != null)
+                bookseats_openRows.setText(String.valueOf("Row 1: x"));
+            if(bookseats_shows != null){
+                bookseats_shows.setItems(FXCollections.observableArrayList(system.getShows()));
+                bookseats_shows.setValue(system.getShows().get(0));
+            }
+        }
     }
 }
